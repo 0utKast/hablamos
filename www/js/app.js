3a415e2b5ff8d79484b81a18bc8d2894db0b76d4
OutKast <educaleft@gmail.com>
2015-10-25 19:05:03 +0000

    119creadoconfig
    creado archivo config.js y eliminado de app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,21 +7,6 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('hablamos', ['ionic', 'hablamos.controllers', 'hablamos.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
 
 
 });
