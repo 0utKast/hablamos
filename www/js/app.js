d7f90a9b45a4cabd5e48132591b26d9a79f3b69f
OutKast <educaleft@gmail.com>
2015-10-25 19:07:37 +0000

    120addstyleenapp.js
    Camnbio estilo en app.js

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('hablamos', ['ionic', 'hablamos.controllers', 'hablamos.services'])
-
-
-
-});
+angular
+  .module('hablamos', [
+    'ionic',
+    'hablamos.services'
+  ]);
